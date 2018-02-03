5412633ecdc014e8d7e1ac372a0809dd7b3ed531
Zxj <defend52y1ssim@vip.qq.com>
2018-02-03 15:19:41 +0800

    test02----Hello World

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        eat();
+        System.out.println("啊哈哈");
+    }
+
+    public static void eat(){
+        System.out.println("你是不是喜欢吃Xxx?");
+    }
+
+}
